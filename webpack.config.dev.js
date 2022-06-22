e4426fdf0353150070330ba8982e72b9683a18a2
SrHatcher <erickomaldonado11@gmail.com>
2022-06-22 15:02:02 -0600

    quiero enviar todo al repo remoto

---
+++ webpack.config.dev.js
@@ -0,0 +1,75 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const dotenv = require('dotenv-webpack')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[contenthash].js'
+    },
+    mode: 'development',
+    resolve: {
+        extensions: ['.js'],
+        alias: {
+            '@utils': path.resolve(__dirname, 'src/utils/'),
+            '@templates': path.resolve(__dirname, 'src/templates/'),
+            '@styles': path.resolve(__dirname, 'src/styles/'),
+            '@images': path.resolve(__dirname, 'src/assets/images'),
+        }
+    },
+    module: {
+        rules:[
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use:{
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.css|.styl$/i,
+                use: [MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'stylus-loader'
+                ]
+            },
+            {
+                test: /\.png$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'static/images/[hash][ext][query]',
+                  }
+            },
+            {
+                test: /\.(woff|woff2)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'static/fonts/[hash][ext][query]',
+                }
+            }
+        ]
+
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            inject: 'body',
+            template: './public/index.html',
+            filename: './indice.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'assets/[name].[contenthash].css'
+        }),
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, "src", "assets/images"),
+                    to: "assets/images"
+                }
+            ]
+        }),
+        new dotenv(), 
+    ],
+}
